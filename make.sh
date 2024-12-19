8b86130f3f0e4aa3f1a9a6cdfcd868e650bf78b9
Artem V. Ageev <artem.v.ageev@gmail.com>
2024-12-19 17:36:12 +0200

    fix github-actions

--- make.sh
+++ make.sh
@@ -55,7 +55,7 @@ function priv_lazbuild
         if ! (lazbuild --no-write-project --recursive --no-write-project --widgetset=qt5 "${REPLY}"); then
             lazbuild --no-write-project --recursive --no-write-project --widgetset=qt5 "${REPLY}" 1>&2
         fi
-    done < <(find 'Lazarus' -type 'f' -name '*.lpi' -and ! -name 'TestRegExp.lpi' | sort)
+    done < <(find 'Lazarus' -type 'f' -name '*.lpi' | grep -vE '(TestRegExp.lpi|stompTest.lpi)' | sort)
 )
 
 function priv_main
