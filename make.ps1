d3e3f37dc4c15997016fa4c8e88b59e02816472e
Artem V. Ageev <artem.v.ageev@gmail.com>
2024-12-19 14:44:19 +0200

    fix github-actions

--- make.ps1
+++ make.ps1
@@ -67,8 +67,10 @@ Function Build-Project {
         }
     }
     Get-ChildItem -Filter '*.lpi' -Recurse -File –Path 'Lazarus' | ForEach-Object {
-        Start-Process -Wait -FilePath $VAR.Cmd -ArgumentList '--no-write-project', '--recursive', $_
-        #& $VAR.Cmd --no-write-project --recursive $_ | Out-Host
+        if ! (& $VAR.Cmd --no-write-project --recursive $_) {
+            & $VAR.Cmd --no-write-project --recursive $_ | Out-Host
+            Throw "Error!"
+        }
     }
 }
 
