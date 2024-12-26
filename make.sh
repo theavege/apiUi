946933bc53f2d37499d06901cd80c60183e26bbb
Artem V. Ageev <artem.v.ageev@gmail.com>
2024-12-26 21:50:29 +0200

    fix github-actions

--- make.sh
+++ make.sh
@@ -16,39 +16,39 @@ function priv_lazbuild
         case ${ID:?} in
             debian | ubuntu)
                 sudo apt-get update
-                sudo apt-get install -y lazarus{-ide-qt5,}
-                printf '\x1b[32mInstall Lazarus.\x1b[0m\n' 1>&2
+                sudo apt-get install -y lazarus{-ide-qt5,} &
                 ;;
         esac
     fi
+    if [[ -f '.gitmodules' ]]; then
+        git submodule update --init --recursive --force --remote &
+    fi
+    wait
     declare -rA VAR=(
         [src]='Lazarus'
         [use]='use'
         [pkg]='use/components.txt'
     )
     if [[ -d "${VAR[use]}" ]]; then
-        if [[ -f '.gitmodules' ]]; then
-            git submodule update --init --recursive --force --remote
-        fi
         if [[ -f "${VAR[pkg]}" ]]; then
             while read -r; do
                 if [[ -n "${REPLY}" ]] &&
+                    ! [[ -d "${VAR[use]}/${REPLY}" ]] &&
                     ! (lazbuild --verbose-pkgsearch "${REPLY}") &&
-                    ! (lazbuild --add-package "${REPLY}") &&
-                    ! [[ -d "${VAR[use]}/${REPLY}" ]]; then
+                    ! (lazbuild --add-package "${REPLY}"); then
                         declare -A TMP=(
                             [url]="https://packages.lazarus-ide.org/${REPLY}.zip"
+                            [dir]="${VAR[use]}/${REPLY}"
                             [out]=$(mktemp)
                         )
-                        wget --quiet --output-document "${TMP[out]}" "${TMP[url]}" >/dev/null
-                        unzip -o "${TMP[out]}" -d "${VAR[use]}/${REPLY}"
+                        wget --quiet --output-document "${TMP[out]}" "${TMP[url]}"
+                        unzip -o "${TMP[out]}" -d "${TMP[dir]}"
                         rm --verbose "${TMP[out]}"
-                        printf '\x1b[32m\tdownload package %s\x1b[0m\n' "${REPLY}" 1>&2
                     fi
             done < "${VAR[pkg]}"
         fi
         find "${VAR[use]}" -type 'f' -name '*.lpk' -print -exec \
-            lazbuild --add-package-link {} +
+            lazbuild --add-package-link {} + 1>&2
     fi
     declare -i errors=0
     while read -r; do
@@ -56,15 +56,15 @@ function priv_lazbuild
             [out]=$(mktemp)
         )
         if (lazbuild --build-all --recursive --no-write-project --build-mode='release' --widgetset='qt5' "${REPLY}" > "${TMP[out]}"); then
-            printf '\x1b[32m\t[%s]\tbuild project\t%s\x1b[0m\t' "${?}" "${REPLY}"
+            printf '\x1b[32m\t[%s]\t%s\x1b[0m\n' "${?}" "${REPLY}"
             grep --color='always' 'Linking' "${TMP[out]}"
         else
-            printf '\x1b[31m\t[%s]\tbuild project\t%s\x1b[0m\n' "${?}" "${REPLY}"
+            printf '\x1b[31m\t[%s]\t%s\x1b[0m\n' "${?}" "${REPLY}"
             grep --color='always' --extended-regexp '(Error|Fatal):' "${TMP[out]}"
             ((errors+=1))
         fi 1>&2
-        rm --verbose "${TMP[out]}"
-    done < <(find "${VAR[src]}" -type 'f' -name '*.lpi' | grep -vE '(backup|nsSql|RegExpr|Xml|wsdlStub|StompInterface)' | sort)
+        rm "${TMP[out]}"
+    done < <(find "${VAR[src]}" -type 'f' -name '*.lpi' | sort)
     exit "${errors}"
 )
 
